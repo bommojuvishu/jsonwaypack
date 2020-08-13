7726dddea27499cdf83a108e7c47bb5a77c996df
vishwanath <bommojuvishu@gmail.com>
2020-08-13 21:37:15 +0530

    added all the methods

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jsonway",
-    version="0.0.5",
+    version="1.0.1",
     author="Vishwanath",
     author_email="justvishu91@gmail.com",
     description="complete tool for the JSON",
